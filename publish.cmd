6ac979d130e065fb5264963ff679db755e452db5
Geoff Affleck <GAffleck@rbauction.com>
2017-12-13 11:30:15 -0800

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=OnboardingRoboto-Web-Deploy.pubxml -p:Password=hsL4gN3srThczWdk7s8P4uACWlZZNB4lToZeqs63TqDZQMgHcX0QN3FvbgCN
+
