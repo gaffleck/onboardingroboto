533fa5e971ff5f80b41dc683176229fa36ba80cb
Geoff Affleck <GAffleck@rbauction.com>
2017-12-13 14:02:36 -0800

    playing with data

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -7,6 +7,8 @@ using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 using dataaccess;
 using System.Linq;
+using Microsoft.Bot.Connector;
+using System.Collections.Generic;
 
 namespace Microsoft.Bot.Sample.LuisBot
 {
@@ -16,10 +18,24 @@ namespace Microsoft.Bot.Sample.LuisBot
     {
         public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(ConfigurationManager.AppSettings["LuisAppId"], ConfigurationManager.AppSettings["LuisAPIKey"])))
         {
+            
         }
+
+        //public Activity Activity { get; set; }
 
         [LuisIntent("None")]
         public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+           
+
+            await context.PostAsync("What do you want to do?"); //
+            context.Wait(MessageReceived);
+        }
+
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
+        [LuisIntent("Fetch Tasks")]
+        public async Task MyIntent(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
         {
             var db = new BotDbContext();
 
@@ -27,21 +43,35 @@ namespace Microsoft.Bot.Sample.LuisBot
 
             var msg = $"Here is what you need to do: ";
 
-            foreach (var t in tasks) {
-                msg = msg + string.Format("\n {0}",t.TaskName);
+            foreach (var t in tasks)
+            {
+                msg = msg + string.Format("\n {0}", t.TaskName);
             }
+            //IMessageActivity act = await activity;
+            //var reply = act.CreateReply();
+
+
+            //reply.Type = ActivityTypes.Message;
+            //reply.TextFormat = TextFormatTypes.Plain;
+
+            //reply.SuggestedActions = new SuggestedActions()
+            //{
+            //    Actions = new List<CardAction>()
+            //   {
+            //    new CardAction(){ Title = "Blue", Type=ActionTypes.ImBack, Value="Blue" },
+            //    new CardAction(){ Title = "Red", Type=ActionTypes.ImBack, Value="Red" },
+            //    new CardAction(){ Title = "Green", Type=ActionTypes.ImBack, Value="Green" }
+            //}
+            //};
+
+            //await context.PostAsync(reply); //
+
 
             await context.PostAsync(msg); //
-            context.Wait(MessageReceived);
-        }
-
-        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
-        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
-        [LuisIntent("MyIntent")]
-        public async Task MyIntent(IDialogContext context, LuisResult result)
-        {
-            await context.PostAsync($"You have reached the MyIntent intent. You said: {result.Query}"); //
+                        
             context.Wait(MessageReceived);
         }
     }
+
+    
 }
